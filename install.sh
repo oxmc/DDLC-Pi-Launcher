b380762786d9417db9922d228b1524c5e839a789
oxmc <67136658+oxmc@users.noreply.github.com>
2021-02-02 14:26:37 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,37 +7,24 @@ echo "Downloading dependencies..."
 apt-get update
 apt-get install -y git python3 python yad renpy
 # Aks user if they have a copy of DDLC
-#If yad exists, use a yad dialog
-if [ -f /usr/bin/yad ];then
-  yad --no-click --separator='\n' \
+yad --no-click --separator='\n' \
 	--title='DDLC-Pi-Launcher installer' --center --window-icon="$DIRECTORY/icons/logo.png" \
 	--text=" Do you have a copy of DDLC?" \
-	--button="No!$DIRECTORY/icons/exit.png:1" \
-	--button="Yes!$DIRECTORY/icons/check.png:0" \
-	--image="$DIRECTORY/icons/install.png" \
+	--button="No!$HOME/DDLC-Pi-Launcher/icons/exit.png:1" \
+	--button="Yes!$HOME/DDLC-Pi-Launcher/icons/check.png:0" \
+	--image="$HOME/DDLC-Pi-Launcher/icons/download.png" \
 	--no-selection 2>/dev/null
-  button=$?
-else #if yad does not exist then fallback to zenity
-  zenity --title='DDLC-Pi-Launcher installer' --window-icon="$DIRECTORY/icons/logo.png" \
-  	--text=" Do you have a copy of DDLC?" \
-  	--ok-label=Yes! --cancel-label=No \
-  	--hide-header 2>/dev/null
-  button=$?
-fi
+button=$?
 if [ ! $button -eq 0 ];then
   echo "User does not have a copy of DDLC."
-  if [ -f /usr/bin/yad ];then
-    yad --no-click --separator='\n' \
+  yad --no-click --separator='\n' \
 	--title='DDLC-Pi-Launcher installer' --center --window-icon="${DIRECTORY}/icons/logo.png" \
 	--text=" Do you want me to download a copy of DDLC?" \
 	--button="No!${DIRECTORY}/icons/exit.png:1" \
 	--button="Yes!${DIRECTORY}/icons/check.png:0" \
 	--image="${DIRECTORY}/icons/install.png" \
 	--no-selection 2>/dev/null
-  else #if yad does not exist
-    echo "Do you want me to download a copy of DDLC? y/n"
-    read DLDDLC
-  fi
+  DLDDLC="$?"
   if [ $DLDDLC == "y" or $button == "0" ]
     echo "Downloading DDLC..."
     wget https://download1499.mediafire.com/anzlui69h0dg/vrlt8gx2ii4j7k3/ddlc-win.zip || error "Unable to download DDLC!"
@@ -45,18 +32,11 @@ if [ ! $button -eq 0 ];then
   fi
 else
 echo "User has a copy of DDLC."
-if [ -f /usr/bin/yad ];then
-    yad --no-click --file="$HOME" --separator='\n' \
+yad --no-click --file="$HOME" --separator='\n' \
 	--title='DDLC-Pi-Launcher installer' --center --window-icon="${DIRECTORY}/icons/logo.png" \
 	--text=" Please select the folder containing DDLC." \
 	--button="No!${DIRECTORY}/icons/exit.png:1" \
 	--button="Yes!${DIRECTORY}/icons/check.png:0" \
 	--image="${DIRECTORY}/icons/install.png" \
 	--no-selection 2>/dev/null
-  else #if yad does not exist
-    echo "Please type the path to the DDLC folder."
-    read DDLCFOLDER
-  fi
-  echo "Please type the path to the DDLC folder."
-  read DDLCFOLDER
 fi
