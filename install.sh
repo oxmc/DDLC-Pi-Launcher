74b18cb6c8139d5cac36c32cec309e4e28f786ac
oxmc <67136658+oxmc@users.noreply.github.com>
2021-02-02 14:00:13 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define variables used in this script
-DIRECTORY = "$(dirname $0)/DDLC-Pi-Launcher"
+DIRECTORY = "$HOME/DDLC-Pi-Launcher"
 # Download dependencies
 echo "Downloading dependencies..."
 apt-get update
@@ -35,13 +35,28 @@ if [ ! $button -eq 0 ];then
 	--image="${DIRECTORY}/icons/install.png" \
 	--no-selection 2>/dev/null
   else #if yad does not exist
-    echo "Do you want me to download a copy of DDLC?"
+    echo "Do you want me to download a copy of DDLC? y/n"
     read DLDDLC
   fi
+  if [ $DLDDLC == "y" or $button == "0" ]
+    echo "Downloading DDLC..."
+    wget https://download1499.mediafire.com/anzlui69h0dg/vrlt8gx2ii4j7k3/ddlc-win.zip || error "Unable to download DDLC!"
+    echo "Downloaded DDLC!"
+  fi
+else
+echo "User has a copy of DDLC."
+if [ -f /usr/bin/yad ];then
+    yad --no-click --file="$HOME" --separator='\n' \
+	--title='DDLC-Pi-Launcher installer' --center --window-icon="${DIRECTORY}/icons/logo.png" \
+	--text=" Please select the folder containing DDLC." \
+	--button="No!${DIRECTORY}/icons/exit.png:1" \
+	--button="Yes!${DIRECTORY}/icons/check.png:0" \
+	--image="${DIRECTORY}/icons/install.png" \
+	--no-selection 2>/dev/null
+  else #if yad does not exist
     echo "Please type the path to the DDLC folder."
     read DDLCFOLDER
-
-    
-else
-  echo "User has a copy of DDLC."
+  fi
+  echo "Please type the path to the DDLC folder."
+  read DDLCFOLDER
 fi
